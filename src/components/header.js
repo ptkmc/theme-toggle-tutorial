80ab02b5838a3c46ee035e145a5cf1b106aa30fe
ptkmc <23647036+ptkmc@users.noreply.github.com>
2019-09-30 00:01:40 -0500

    add margin styling to Switch

--- src/components/header.js
+++ src/components/header.js
@@ -32,18 +32,20 @@ const Header = ({ siteTitle, theme }) => (
         </Link>
 
       </h1>
-      <Switch
-        onChange={() =>
-          theme.updateTheme(theme.name === "light" ? "dark" : "light")
-        }
-        checked={theme.name === "dark"}
-        onColor="#222"
-        offColor="#333"
-        checkedIcon={<img src={moonIcon} alt="moon icon" />}
-        uncheckedIcon={<img src={sunIcon} alt="sun icon" />}
-        boxShadow="0 0 2px 3px #B38CD9"
-        activeBoxShadow="0 0 2px 3px #dfb3e6"
-      />
+      <div style={{ marginTop: '1em', marginBottom: '-1em' }}>
+        <Switch
+          onChange={() =>
+            theme.updateTheme(theme.name === "light" ? "dark" : "light")
+          }
+          checked={theme.name === "dark"}
+          onColor="#222"
+          offColor="#333"
+          checkedIcon={<img src={moonIcon} alt="moon icon" />}
+          uncheckedIcon={<img src={sunIcon} alt="sun icon" />}
+          boxShadow="0 0 2px 3px #B38CD9"
+          activeBoxShadow="0 0 2px 3px #dfb3e6"
+        />
+      </div>
     </div>
   </header >
 )
